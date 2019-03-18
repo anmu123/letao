2797f040bd85ba9da79b4a8127468c8b021011af
anmu123 <1606423847@qq.com>
2019-03-18 21:54:19 +0800

    登录界面完成

---
+++ admin/js/login.js
@@ -0,0 +1,83 @@
+$(function () {
+
+
+    //使用表单校验插件
+    $('.form').bootstrapValidator({
+        //1. 指定不校验的类型，默认为[':disabled', ':hidden', ':not(:visible)'],可以不设置
+        excluded: [':disabled', ':hidden', ':not(:visible)'],
+
+        //2. 指定校验时的图标显示，默认是bootstrap风格
+        feedbackIcons: {
+            valid: 'glyphicon glyphicon-ok',
+            invalid: 'glyphicon glyphicon-remove',
+            validating: 'glyphicon glyphicon-refresh'
+        },
+
+        //3. 指定校验字段
+        fields: {
+            //校验用户名，对应name表单的name属性
+            username: {
+                validators: {
+                    //不能为空
+                    notEmpty: {
+                        message: '用户名不能为空'
+                    },
+                    //长度校验
+                    stringLength: {
+                        min: 3,
+                        max: 6,
+                        message: '用户名长度必须在3到6之间'
+                    },
+
+                }
+            },
+            password: {
+                validators: {
+                    //不能为空
+                    notEmpty: {
+                        message: '密码不能为空'
+                    },
+                    //长度校验
+                    stringLength: {
+                        min: 6,
+                        max: 12,
+                        message: '用户名长度必须在6到12之间'
+                    },
+
+                }
+            },
+        }
+
+    });
+
+
+
+
+
+
+    $(".form").on('success.form.bv', function (e) {
+        e.preventDefault();
+        //使用ajax提交逻辑
+
+        $.ajax({
+            type: 'post',
+            url: '/employee/employeeLogin',
+            data:$(".form").serialize(),
+            success:function(info){
+                if(info.error === 1000){
+                    alert('用户名错误')
+                }
+                if(info.error === 1001){
+                    alert('密码错误')
+                }
+                if(info.success){
+                   location.href = 'index.html'
+                }
+            }
+        })
+    })
+
+    
+
+
+})
